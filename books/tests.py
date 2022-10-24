4bde3ca8e5436f708591a6a5a9e44a3dd4b8cab4
Daniil4949 <daniilkimstachp@gmail.com>
2022-10-24 07:55:37 +0300

    New testcase for the feedback form was added.

--- books/tests.py
+++ books/tests.py
@@ -1,5 +1,6 @@
 from django.test import Client
-from django.test import TestCase
+from django.test import TestCase, SimpleTestCase
+from books.forms import FeedbackForm
 from django.urls import reverse
 
 from books.models import Feedback
@@ -23,8 +24,23 @@ class FeedbackTestCase(TestCase):
 
 class MainPageTestCase(TestCase):
     """Testcase for the main page and for the right status code(200)"""
-    def test_main_page(self):
-        client = Client()
-        response = client.get(reverse('home'))
+    def setUp(self) -> None:
+        self.client = Client
+        self.list_url = reverse('home')
+
+    def test_main_page(self) -> None:
+        response = self.client.get(reverse('home'), {})
         self.assertEqual(response.status_code, 200)
 
+
+class FeedbackFormTestCase(SimpleTestCase):
+    """Text field of the model 'Feedback' is not nullable"""
+    def test_expense_form_valid_data(self) -> None:
+        form = FeedbackForm(data={
+            'text': 'I adore this site!'
+        })
+        self.assertTrue(form.is_valid())
+
+    def test_wrong_data_form(self) -> None:
+        form = FeedbackForm(data={})
+        self.assertFalse(form.is_valid())
