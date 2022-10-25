1253624a85582cc134ee0ddf834a7be163ae8ac0
Daniil4949 <daniilkimstachp@gmail.com>
2022-10-25 10:46:44 +0300

    Tried to add useful tests

--- books/tests.py
+++ books/tests.py
@@ -2,24 +2,7 @@ from django.test import Client
 from django.test import TestCase, SimpleTestCase
 from books.forms import FeedbackForm
 from django.urls import reverse
-
 from books.models import Feedback
-
-
-class FeedbackTestCase(TestCase):
-    def setUp(self) -> None:
-        """Creating test objects in the database"""
-        Feedback.objects.create(text="I really adore this application")
-        Feedback.objects.create(text="I do not know why it was created at all")
-
-    def testTheMethod(self):
-        """Testing the '__str__' method created in books.models'"""
-        first_fb = Feedback.objects.get(text="I really adore this application")
-        second_fb = Feedback.objects.get(text="I do not know why it was created at all")
-        self.assertEqual("The text of the selected user's review: I really adore this application",
-                         f"The text of the selected user's review: {first_fb.text}")
-        self.assertEqual("The text of the selected user's review: I do not know why it was created at all",
-                         f"The text of the selected user's review: {second_fb.text}")
 
 
 class MainPageTestCase(TestCase):
@@ -44,3 +27,4 @@ class FeedbackFormTestCase(SimpleTestCase):
     def test_wrong_data_form(self) -> None:
         form = FeedbackForm(data={})
         self.assertFalse(form.is_valid())
+
--- cart/tests.py
+++ cart/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.test import Client
+from django.test import TestCase, SimpleTestCase
+from cart.forms import SearchBookForm
+from django.urls import reverse
 
-# Create your tests here.
+
+class SearchformTestCase(TestCase):
+    def test_form_valid_data(self):
+        form = SearchBookForm(data={
+            "title": "Martin Eden"
+        })
+        self.assertTrue((form.is_valid()))
